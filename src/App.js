1d7acec3812f752926cccf16952e51632cfe1b44
RoronoaZoro1925 <sarthak1999vyas@gmail.com>
2024-02-24 15:48:38 +0530

    Clean-App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import './App.css';
-import FirstComponent from './components/learning-examples/FirstComponent'
-import SecondComponent from './components/learning-examples/SecondComponent'
-import ThirdComponent from './components/learning-examples/ThirdComponent';
-import FourthComponent from './components/learning-examples/FourthComponent';
-import { FifthComponent } from './components/learning-examples/FourthComponent';
+import LearningComponent from './components/learning-examples/LearningComponent';
 
-function App() {
-  return (
+function App() 
+{
+  return(
     <div className="App">
-      <FirstComponent></FirstComponent>
-      <SecondComponent></SecondComponent>
-      <ThirdComponent></ThirdComponent>
-      <FourthComponent></FourthComponent>
-      <FifthComponent/>
+    <LearningComponent/>
+
     </div>
-  );
+  )
+  
 }
+
+
 export default App;
